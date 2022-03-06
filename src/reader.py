459ba175d3d9b44e0f0021c952cd2896a0ec0eed
Simão Pedro Sá Cunha <61991247+simaocunha71@users.noreply.github.com>
2022-03-06 16:49:03 +0000

    leitura da primeira linha
    Co-Authored-By: Gonçalo Braz Afonso <61985783+brazafonso@users.noreply.github.com>

--- src/reader.py
+++ src/reader.py
@@ -1,8 +1,26 @@
 import sys
 import re
 
-pattern = r'(?:\"(?P<id>.+)\",)(?:\"(?P<nome>.+)\",)(?:\"(?P<curso>.+)\",)(?:(\d+),)(?:(\d+),)(?:(\d+),)(\d+)' #a mudar
-pattern_file = re.compile(pattern)
+def get_columns_names(string):
+    columns = []
+    reg_exp = r'((((?P<nome>(\w|[À-ÿ])+)((?P<repetidos>{\d(,\d)?})(::(?P<metodo>(\w+)))?)?),?))'
+    compila = re.compile(reg_exp)
+    matches = compila.finditer(string)
+    for match in matches:
+        if(match.group("metodo") and match.group("repetidos") and match.group("nome")):
+            columns.append((match.group("nome"),match.group("repetidos"), match.group("metodo")))
+
+        elif(match.group("repetidos") and match.group("nome")):
+            columns.append((match.group("nome"), match.group("repetidos")))
+
+        elif(match.group("nome")):
+            columns.append(match.group("nome"))
+
+    
+    print(columns)
+
+pattern_content = r'(?:\"(?P<id>.+)\",)(?:\"(?P<nome>.+)\",)(?:\"(?P<curso>.+)\",)(?:(\d+),)(?:(\d+),)(?:(\d+),)(\d+)' #a mudar
+pattern_file = re.compile(pattern_content)
 
 #ficheiro a abrir como input
 filename_to_open = sys.argv[1]
@@ -14,11 +32,17 @@ file_json = open(filename_to_save,'w')
 
 
 line_to_read = file_csv.readline()
+get_columns_names("Número,Nome,Curso,Notas{3,5}::sum,,,,,")
+
+"""
 while (line_to_read != ""): #nao deteta EOF
     valid_line = pattern_file.search(line_to_read) #tenta comparar se a linha dá match
     if (valid_line):
-        file_json.write(valid_line) #escreve no ficheiro
+        file_json.write(valid_line.group()) #escreve no ficheiro
+        file_json.write("\n")
     line_to_read = file_csv.readline() #le proxima linha
+
+"""
 
 
 

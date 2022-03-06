282bf5f6d213e60e03626578357d77f288f83a43
Simão Pedro Sá Cunha <61991247+simaocunha71@users.noreply.github.com>
2022-03-06 12:32:37 +0000

    inicio da aplicação das expressoes regulares

--- src/reader.py
+++ src/reader.py
@@ -1,4 +1,8 @@
 import sys
+import re
+
+pattern = r'(?:\"(?P<id>.+)\",)(?:\"(?P<nome>.+)\",)(?:\"(?P<curso>.+)\",)(?:(\d+),)(?:(\d+),)(?:(\d+),)(\d+)' #a mudar
+pattern_file = re.compile(pattern)
 
 #ficheiro a abrir como input
 filename_to_open = sys.argv[1]
@@ -9,10 +13,12 @@ filename_to_save = filename_to_open.replace("csv","json") #muda terminação de 
 file_json = open(filename_to_save,'w')
 
 
-line = file_csv.readline()
-while (line != ""): #nao deteta EOF
-    file_json.write(line) #do something
-    line = file_csv.readline() #le proxima linha
+line_to_read = file_csv.readline()
+while (line_to_read != ""): #nao deteta EOF
+    valid_line = pattern_file.search(line_to_read) #tenta comparar se a linha dá match
+    if (valid_line):
+        file_json.write(valid_line) #escreve no ficheiro
+    line_to_read = file_csv.readline() #le proxima linha
 
 
 

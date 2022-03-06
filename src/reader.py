c3d62289376ef696be5986ca774d1efb54dc6a3a
Simão Pedro Sá Cunha <61991247+simaocunha71@users.noreply.github.com>
2022-03-06 12:23:08 +0000

    leitura linha a linha

--- src/reader.py
+++ src/reader.py
@@ -5,15 +5,15 @@ filename_to_open = sys.argv[1]
 
 file_csv = open(filename_to_open,"r")
 
-lines = file_csv.readlines()
-
-for line in lines:
-    print(line)
-
 filename_to_save = filename_to_open.replace("csv","json") #muda terminação de csv para json
 file_json = open(filename_to_save,'w')
 
-file_json.write("Ja meti aqui qq coisa")
+
+line = file_csv.readline()
+while (line != ""): #nao deteta EOF
+    file_json.write(line) #do something
+    line = file_csv.readline() #le proxima linha
+
 
 
 file_csv.close()
